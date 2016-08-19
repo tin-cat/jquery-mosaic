d9e768e5320f150d426ac1cca20bf8c613677189
Lorenzo <lorenzo@tin.cat>
2016-08-19 12:09:08 +0000

    Solved bug with examples

--- examples/res/pattern.php
+++ examples/res/pattern.php
@@ -3,8 +3,8 @@
 	<title><?= $title ? $title : "jquery-mosaic plugin example" ?></title>
 	<link rel="stylesheet" type="text/css" href="res/css/main.css"/>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="../jquery.mosaic.css"/>
-	<script type="text/javascript" src="../jquery.mosaic.js"></script>
+	<link rel="stylesheet" type="text/css" href="jquery.mosaic.css"/>
+	<script type="text/javascript" src="jquery.mosaic.js"></script>
 </head>
 <body>
 
