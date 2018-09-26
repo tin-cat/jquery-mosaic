b3e350513993ca5a150942ed2038215c9895db8d
Lorenzo <lorenzo@tin.cat>
2018-09-26 09:53:05 +0000

    Improved SEO for the website and examples

--- examples/res/pattern.php
+++ examples/res/pattern.php
@@ -1,10 +1,14 @@
 <!DOCTYPE HTML>
 <head>
-	<title><?= $title ? $title : "jquery-mosaic plugin example" ?></title>
+	<title><?= $title ? $title : "jQuery Mosaic" ?></title>
+	<meta name="description" content="<?= $description ? $description : "A jQuery plugin for building responsive mosaics of images or any other content fitted to match heights in multiple rows while maintaining aspect ratios." ?>" />
+	<meta name="keywords" content="<?= $keywords ? $keywords : "jquery,mosaic,plugin,javascript,gallery,photos,masonry" ?>" />
+	<meta name="distribution" CONTENT="global" />
+	<link rel="canonical" href="https://jquery-mosaic.tin.cat" />
 	<link rel="stylesheet" type="text/css" href="res/css/main.css?v=5"/>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0" />
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="jquery.mosaic.css?v=5"/>
-	<meta http-equiv="X-UA-Compatible" content="IE=9">
 	<script type="text/javascript" src="jquery.mosaic.js?v=4"></script>
 	<link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
 </head>
